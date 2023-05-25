50629e1243bf8fe132af380c6321792b70a93fd4
1213-9 <1213-9@1213-9-PC>
2023-05-25 14:04:21 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            //aaaaaaaaaaaa randoooom
+            Random r = new Random(6);
+            int rnd = r.Next();
+            if (rnd > 3)
+            {
+                if (!string.IsNullOrEmpty(txtElem.Text) && !lstElem.Items.Contains(txtElem.Text))
+                {
+                    if (MessageBox.Show("Вы действительно хотите добавить новый элемент?", "Добавление в список",
+                        MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                    {
+                        lstElem.Items.Add(txtElem.Text);
+                        txtElem.Clear();
+                    }
+                }
+                else MessageBox.Show("Надо что-нибудь ввести!", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else MessageBox.Show("Надо что-нибудь ввести!", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            lstElem.Items.Clear();
+        }
+    }
+}
